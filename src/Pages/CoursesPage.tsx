68111b908ab1e8dff9d8454bbe8878e22a3fab77
vishal kumar ojha <vishal.23bhi10067@vitbhopal.ac.in>
2025-02-07 23:36:24 +0530

    Updated responsiveness

--- src/Pages/CoursesPage.tsx
+++ src/Pages/CoursesPage.tsx
@@ -1,15 +1,19 @@
-import { CourseSidebar } from "@/components/course/sidebar"
-import { A2ZSheet } from "@/components/sheets/A2ZSheet"
-import { sheets } from "@/lib/course-data"
+import { CourseSidebar } from "@/components/course/sidebar";
+import { A2ZSheet } from "@/components/sheets/A2ZSheet";
+import { sheets } from "@/lib/course-data";
 
 export default function CoursesPage() {
   return (
     <div className="flex min-h-screen bg-arno-dark-900">
-      <CourseSidebar sheets={sheets} />
-      <div className="flex-1 p-8">
+      {/* Sidebar: Hidden on small screens, appears on medium and above */}
+      <div className="hidden md:block w-64">
+        <CourseSidebar sheets={sheets} />
+      </div>
+
+      {/* Main content with padding to avoid overlap */}
+      <div className="flex-1 p-4 md:p-8">
         <A2ZSheet />
       </div>
     </div>
-  )
+  );
 }
-
